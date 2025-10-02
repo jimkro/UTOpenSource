f8b34c0920c95c3af69887bd2649fbbccc60fcb5
ShowMaker <a0979810349@gmail.com>
2025-10-02 10:01:25 +0800

    fix : PB傳錯
    原本PB是DS5的改回DS4

--- Algorithm/暑期DS練習題/DS4/PB.cpp
+++ Algorithm/暑期DS練習題/DS4/PB.cpp
@@ -1,58 +1,61 @@
 #include <bits/stdc++.h>
-#define llg long long
+
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
     
-    // crewmate
-    vector<int> arr; int x;// x : 暫時讀取用的(少用一點變數或許會省一點?)
-
-    string line;
-    if (!getline(cin, line)) return 0;
-    stringstream ss(line); while (ss >> x) arr.push_back(x);
-    
-
-    int n = arr.size();
-
-    // 計算最大子陣列和 (By Kadane's Algorithm)
-    llg max_sum = LLONG_MIN, curr = 0;
-    for (int i = 0; i < n; i++) {
-        curr = max((llg)arr[i], curr + arr[i]);
-        max_sum = max(max_sum, curr);
-    }
-
-    // prefix sum, prefix[n] = arr[0] + ... + arr[n-1]
-    vector<llg> prefix(n + 1, 0);
-    for (int i = 1; i <= n; i++) prefix[i] = prefix[i-1] + arr[i-1];
-
-    // 查詢各輸入
-    while (cin >> x) {
-        int Q = x;
-
-        // 剪枝 (提速用?)
-        if (Q > max_sum) {
-            cout << "0\n";
-            continue;
+    int V,E,T;
+    while(cin >> V >> E >> T && (V != 0 || E !=0 || T != 0)){
+        vector<priority_queue<int, vector<int>, greater<int>>> graph(V+1);
+        vector<bool> visited(V+1,false);
+        for(int i=0;i<E;i++){
+            int v1,v2;
+            cin >> v1 >> v2;
+            graph[v1].push(v2);
+            graph[v2].push(v1);
         }
 
-        // Monotonic Queue 維護可能起點
-        deque<int> dq;
-        int ans = n;
+        if(T == 0){ // BFS
+            queue<int> q;
+            q.push(1);
 
-        for (int i = 0; i <= n; i++) {
-            while (!dq.empty() && prefix[i] - prefix[dq.front()] >= Q) {
-                ans = min(ans, i - dq.front());
-                dq.pop_front();
+            while(!q.empty()){
+                int curr = q.front();
+                q.pop();
+
+                if(visited[curr]) continue;
+                visited[curr] = true;
+
+                cout << curr << " ";
+
+                while(!graph[curr].empty()){
+                    q.push(graph[curr].top());
+                    graph[curr].pop();
+                }
             }
-            while (!dq.empty() && prefix[i] <= prefix[dq.back()]) {
-                dq.pop_back();
+            cout << "\n";
+        }else{ // DFS
+            stack<int> st;
+            st.push(1);
+
+            while(!st.empty()){
+                int curr = st.top();
+                st.pop();
+
+                if(visited[curr]) continue;
+                visited[curr] = true;
+
+                cout << curr << " ";
+
+                while(!graph[curr].empty()){
+                    st.push(graph[curr].top());
+                    graph[curr].pop();
+                }
             }
-            dq.push_back(i);
+            cout << "\n";
         }
-
-        cout << ans << "\n";
+        
     }
-
     return 0;
 }
