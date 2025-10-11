2a9a188c9f331e3fbbafc817097399166a4ad594
ShowMaker <a0979810349@gmail.com>
2025-10-11 23:07:24 +0800

    refactor : 把最後多加一格讓他自然賣出

--- CS/Contest/NCPC/111_final/PC/PC.cpp
+++ CS/Contest/NCPC/111_final/PC/PC.cpp
@@ -11,21 +11,17 @@ int main(){
         cin >> n;
         vector<int> stocks(n);
         for(int i = 0; i < n; i++) cin >> stocks[i];
+        stocks.push_back(INT_MIN);
         int l = 0;
         long long ans = 0;
-        for(int r = 1; r < n; r++){
+        
+        for(int r = 1; r <= n; r++){
             if(stocks[r] <= stocks[r-1]){
-                if(r-1 != l){
-                    ans += stocks[r-1] - stocks[l];
-                }
+                ans += stocks[r-1] - stocks[l];
                 l = r;
             }
         }
 
-        if(n - 2 >= 0 && stocks[n-1] > stocks[n-2] && n-1 != l){
-            ans += stocks[n-1] - stocks[l];
-        }
-        
         cout << ans << "\n";
     }
 }
