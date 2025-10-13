2f29b806fc003a385ad2b7036de2ebe1ea6ae004
ShowMaker <a0979810349@gmail.com>
2025-10-13 19:18:57 +0800

    feat : P7( 反向Floyd)

--- CS/Contest/CPE/考古系列/P2025_09_30/P7_Geonosis/main.cpp
+++ CS/Contest/CPE/考古系列/P2025_09_30/P7_Geonosis/main.cpp
@@ -1,39 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    int T; cin >> T;
     
-    int T, n; cin >> T;
-
-    while(cin >> n){
-        vector<vector<int>> adj(n,vector<int>(n,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cin >> adj[i][j];
-            }
-        } 
-
-        for(int k = 0; k < n; k++){
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(i == j) continue;
-                    adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
-                }
-            }
-        }
+    while (T--) {
+        int n; cin >> n;
+        
+        vector<vector<long long>> dist(n, vector<long long>(n));
+        for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) cin >> dist[i][j];
 
         vector<int> order(n);
-        for(int i=0;i<n;i++) cin >> order[i];
+        for (int i = 0; i < n; i++) cin >> order[i];
 
+        vector<bool> added(n);
         long long sum = 0;
-        for(int x : order){
-            for(int i=0;i<n;i++){
-                sum += adj[x][i];
-                adj[i][x] = 0;
+
+        for (int k = 0; k < n; k++) {
+            int x = order[n-k-1]; // 反向加入
+            added[x] = true;
+
+            // Floyd 更新
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(dist[i][x] + dist[x][j] < dist[i][j]) dist[i][j] = dist[i][x] + dist[x][j];
+                }
+            }
+
+            // 計算目前已加入的塔間的最短距離總和
+            for (int i = 0; i < n; i++) {
+                if (!added[i]) continue;
+                for (int j = 0; j < n; j++) {
+                    if (!added[j]) continue;
+                    sum += dist[i][j];
+                }
             }
         }
 
         cout << sum << "\n";
     }
+    return 0;
 }
