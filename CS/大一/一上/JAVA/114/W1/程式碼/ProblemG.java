2b40ff67a1a5d598ef47d86f3fb421f6a3eaa8e4
Lynn <sulynn1220@gmail.com>
2025-11-05 15:08:13 +0800

    perf : 優化算法

--- CS/大一/一上/JAVA/114/W1/程式碼/ProblemG.java
+++ CS/大一/一上/JAVA/114/W1/程式碼/ProblemG.java
@@ -1,25 +1,15 @@
 import java.util.*;
-import java.lang.Math;
 
 public class ProblemG {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            String s = sc.next();
-            long total = 0;
-            long now = 0;
-            int a = Integer.parseInt(s);
-            for (int j = s.length(); j >= 1; j--) {
-                if (a % 10 == a) {
-                    total = a;
-                    break;
-                }
-
-                int result = (int) Math.pow(10, j);
-                int result1 = (int) Math.pow(10, j - 1);
-                now = a % result1;
-                total += (a % result - now) / result1;
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int n = sc.nextInt();
+            int total = 0;
+            while (n > 0) {
+                total += n % 10;
+                n /= 10;
             }
 
             System.out.println(total);
