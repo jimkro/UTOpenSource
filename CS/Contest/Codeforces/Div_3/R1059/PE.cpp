b5dc630c85cb745f4a7421ecda27601619828c48
ShowMaker <a0979810349@gmail.com>
2025-11-18 10:48:35 +0800

    feat : update Div.3 R1059

--- CS/Contest/Codeforces/Div_3/R1059/PE.cpp
+++ CS/Contest/Codeforces/Div_3/R1059/PE.cpp
@@ -1,29 +1,44 @@
+// upsolving AC
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
 void solve(){
     int n, k; cin >> n >> k;
-    vector<int> A(n); for(int i=0;i<n;i++) cin >> A[i];
-    
-    int n1 = A[n-2], n2 = A[n-1];
+    vector<int> v(n);
+    vector<int> cnt(n + 1);
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+        cnt[v[i]]++;
+    }
 
-    for(int i=0;i<k;i++){
-        for(int j = 1; j <= n;j++){
-            if(j != n1 && j != n2){
-                n1 = n2;
-                n2 = j;
-                cout << j << " ";
+    int x = -1;
+    for(int i=1;i<=n;i++){
+        if(cnt[i] == 0){
+            x = i;
+            break;
+        }
+    }
+
+    //三個一循環
+    if(x == -1){ // 是 permutation
+        for(int i=0;i<k;i++) cout << v[n - 3 + (i % 3)] << " "; 
+    }else{ // 不是 permutation
+        int y = -1;
+        for(int i=1;i<=n;i++){
+            if(i != x && i != v[n - 1]){
+                y = i;
                 break;
             }
         }
+        vector<int> cir = {x, y, v[n - 1]};
+        for(int i=0;i<k;i++) cout << cir[i % 3] << " ";
     }
     cout << "\n";
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    int t; cin >> t;
-
-    while(t--) solve();
+    int T; cin >> T;
+    while(T--) solve();
 }   
